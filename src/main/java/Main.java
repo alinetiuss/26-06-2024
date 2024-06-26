eb8b0a51b800a6f405f8310dd1cb00e1c5fd1f18
AlineTiuss <169068304+alinetiuss@users.noreply.github.com>
2024-06-26 20:13:37 -0300

    26/06/2024

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,23 +2,24 @@ import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
-
     Scanner sc = new Scanner(System.in);
 
     System.out.println("Digite um número (inteiro):");
     int numero = sc.nextInt();
 
-    if (numero > 0) {
-      System.out.println("O número é positivo");
-    }
-
+    if (numero>0) {
     if (numero == 0) {
-      System.out.println("O número é posistivo");
+      System.out.printf(" O número escolhido é: positivo");
+    }
+      System.out.println("O número escolhido é: positivo");
     }
 
-    if (numero < 0) {
-      System.out.println("O número é negativo");
-    }
+    if (numero<0) {
+      System.out.println("O número escolhido é: negativo");
+      }
     sc.close();
-  }
-}
+    
+    }
+    
+    }
+  
